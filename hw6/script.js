a1ec581ed2e3b62177182c9415bd3d204ea6cb85
rusbey95 <rusikbey95@gmail.com>
2023-01-25 21:47:20 +0100

    add hw 6

--- hw6/script.js
+++ hw6/script.js
@@ -85,8 +85,26 @@ for (film in myFavoriteFilms) {
 
 // second level
 // task 4 - Клавиатура на Вашем экране! Создайте кнопки(button) со всеми английскими буквами. Застилизуйте их так, чтобы на экране была копия клавиатуры (только английские буквы, цвета и размеры кнопок выбирайте на свой вкус).
+// task 5 - Доработайте предыдущий скрипт так, чтобы при нажатии на кнопку, сверху видеть буквы, на которые нажимает пользователь(имитация клавиатуры). Подсказка: можете использовать параграф.
 const keyboard = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
+const ulKeyboard = document.createElement('ul');
+ulKeyboard.classList.add('keyboard');
+for (let i = 0; i < keyboard.length; i++) {
+    let liKeyboard = document.createElement('li');
+    liKeyboard.innerText = keyboard[i];
+
+    ulKeyboard.append(liKeyboard);
+    body.append(ulKeyboard);
+
+    window.addEventListener('keydown', function(e) {
+        console.log(e.key);
+        if (e.key === keyboard[i]) {
+            liKeyboard.classList.add('active');
+        } else {
+            liKeyboard.classList.remove('active');
+        }
+    });
+}
 
 
-// 5)Доработайте предыдущий скрипт так, чтобы при нажатии на кнопку ,сверху видеть буквы, на которые нажимает пользователь(имитация клавиатуры). Подсказка: можете использовать параграф.
