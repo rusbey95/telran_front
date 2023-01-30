a7d619f263ddb4b94e56addfd211734972047163
rusbey95 <rusikbey95@gmail.com>
2023-01-30 18:54:09 +0100

    add hw7 task1

--- hw7/script.js
+++ hw7/script.js
@@ -4,6 +4,11 @@ const form       = document.querySelector('form');
 const button     = document.querySelector('button');
 const inputTitle = document.querySelector('#product-title');
 const inputPrice = document.querySelector('#product-price');
+const productSumButton = document.querySelector('.show-products-sum');
+
+let ul     = document.createElement('ul');
+let sumBox = document.createElement('div');
+ul.classList.add('products-list');
 
 const productArr = [];
 
@@ -22,21 +27,39 @@ form.onsubmit = (e) => {
 }
 
 function createNode(title, price) {
-    let div = document.createElement('div');
     let h3  = document.createElement('h3');
     let p   = document.createElement('p');
+    let li  = document.createElement('li');
+    li.classList.add('products-item');
 
     h3.innerText = title;
     p.innerText  = price;
-    div.classList.add('product-item');
 
-    div.append(h3, p);
-    products.append(div);
+    li.append(h3, p);
+    ul.append(li);
+    products.append(ul);
 }
 
 function newProducts() {
-    products.innerHTML = '';
-    for (let i = 0; i < products.length; i++) {
-        createNode(products[i].title, products[i].price)
+    ul.innerHTML = '';
+    for (let i = 0; i < productArr.length; i++) {
+        createNode(productArr[i].title, productArr[i].price)
     }
 }
+
+// hw task - 
+function showProductSum() {
+    let productSum = 0;
+
+    for (let i = 0; i < productArr.length; i++) {
+        productSum += +productArr[i].price;
+    }
+
+    sumBox.innerText = productSum;
+    products.append(sumBox);
+}
+
+productSumButton.addEventListener('click', function() {
+    sumBox.innerHTML = '';
+    showProductSum();
+});
